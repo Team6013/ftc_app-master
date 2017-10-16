cfb07d4eea8163e0b9c070604502d4562b6361a3
Roba <abbajabal.roba@jdragonmail.us>
2017-10-15 20:34:08 -0500

    Roba pushed this.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousUsingVuforiaVuMarksByRoba.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousUsingVuforiaVuMarksByRoba.java
@@ -7,6 +7,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.ConceptVuforiaNavigation;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -25,13 +28,45 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 import static com.sun.tools.javac.util.Constants.format;
 
-@Autonomous(name="VuforiaVuMarksIdentification", group ="VuforiaVuMarksIdentification")
+
+/*
+ *           README: Commands made by Roba Abbajabal
+ *--------------------------------------------------------------
+ * forward(motorPower, timeInMilliseconds);
+ * backward(motorPower, timeInMilliseconds);
+ * left(motorPower, timeInMilliseconds);
+ * right(motorPower, timeInMilliseconds);
+ * stop_driving();
+ *
+ * IMPORTANT NOTE: motorPower can range between 0 to 1, 0 being no movement and 1 being full forward. motorPower can be written in decimal numbers. (Ex: 0, 0.4, 1)
+ *                 timeInMilliseconds does not accept decimal/fractional numbers.
+ *                 Start where it says "Begin editing code here." and end where it says "Stop editing code here." DO NOT edit any code outside of those two comments!
+ */
+
+@Autonomous(name="AutonomousUsingVuforiaVuMarksByRoba", group ="AutonomousUsingVuforiaVuMarksByRoba")
 //@Disabled
-public class VuforiaVuMarksIdentification extends LinearOpMode {
+public class AutonomousUsingVuforiaVuMarksByRoba extends LinearOpMode {
 
     VuforiaLocalizer vuforia;
 
-    @Override public void runOpMode() {
+    private ElapsedTime runTime = new ElapsedTime();
+    private DcMotor leftMotor = null;
+    private DcMotor rightMotor = null;
+    private Servo leftClaw = null;
+    private Servo rightClaw = null;
+
+    @Override public void runOpMode() throws InterruptedException {
+
+        telemetry.addData("Status", "This script has been made by Roba! " + "The robot has initialized! ");
+
+        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+        leftClaw = hardwareMap.get(Servo.class, "leftClaw");
+        rightClaw = hardwareMap.get(Servo.class, "rightClaw");
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftClaw.setDirection(Servo.Direction.FORWARD);
+        rightClaw.setDirection(Servo.Direction.REVERSE);
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
@@ -66,7 +101,6 @@ public class VuforiaVuMarksIdentification extends LinearOpMode {
                     double tX = trans.get(0);
                     double tY = trans.get(1);
                     double tZ = trans.get(2);
-
                     double rX = rot.firstAngle;
                     double rY = rot.secondAngle;
                     double rZ = rot.thirdAngle;
@@ -76,17 +110,33 @@ public class VuforiaVuMarksIdentification extends LinearOpMode {
                         telemetry.addData("X =", tX);
                         telemetry.addData("Y =", tY);
                         telemetry.addData("Z =", tZ);
+                        //Begin editing code here.
+                        forward(0.5, 1500);
+
+                        stop_driving();
+                        //Stop editing code here.
+
                     } else if (vuMark == RelicRecoveryVuMark.CENTER) {
                         telemetry.addData("VuMark is ", "Center");
                         telemetry.addData("X =", tX);
                         telemetry.addData("Y =", tY);
                         telemetry.addData("Z =", tZ);
+                        //Begin editing code here.
+                        forward(0.5, 1500);
+
+                        stop_driving();
+                        //Stop editing code here.
 
                     } else if (vuMark == RelicRecoveryVuMark.RIGHT) {
                         telemetry.addData("VuMark is ", "Right");
                         telemetry.addData("X =", tX);
                         telemetry.addData("Y =", tY);
                         telemetry.addData("Z =", tZ);
+                        //Begin editing code here.
+                        forward(0.5, 1500);
+
+                        stop_driving();
+                        //Stop editing code here.
 
                     }
                 }
@@ -102,5 +152,40 @@ public class VuforiaVuMarksIdentification extends LinearOpMode {
 
     String format(OpenGLMatrix transformationMatrix) {
         return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
+    }
+
+    public void forward(double motorPower, long timeInMilliseconds) throws InterruptedException{
+        leftMotor.setPower(motorPower);
+        rightMotor.setPower(motorPower);
+        telemetry.addData("Motors", "The robot's left motor is running at " + motorPower + " for " + timeInMilliseconds + " milliseconds.");
+        telemetry.addData("Motors", "The robot's right motor is running at " + motorPower + " for " + timeInMilliseconds + " milliseconds.");
+        Thread.sleep(timeInMilliseconds);
+    }
+    public void backward(double motorPower, long timeInMilliseconds) throws InterruptedException{
+        leftMotor.setPower(motorPower);
+        rightMotor.setPower(motorPower);
+        telemetry.addData("Motors", "The robot's left motor is running at " + motorPower + " for " + timeInMilliseconds + " milliseconds.");
+        telemetry.addData("Motors", "The robot's right motor is running at " + motorPower + " for " + timeInMilliseconds + " milliseconds.");
+        Thread.sleep(timeInMilliseconds);
+
+    }
+    public void left(double motorPower, long timeInMilliseconds) throws InterruptedException{
+        leftMotor.setPower(motorPower);
+        rightMotor.setPower(-motorPower);
+        telemetry.addData("Motors", "The robot's left motor is running at " + motorPower + " for " + timeInMilliseconds + " milliseconds.");
+        telemetry.addData("Motors", "The robot's right motor is running at " + -motorPower + " for " + timeInMilliseconds + " milliseconds.");
+        Thread.sleep(timeInMilliseconds);
+    }
+    public void right(double motorPower, long timeInMilliseconds) throws InterruptedException{
+        leftMotor.setPower(-motorPower);
+        rightMotor.setPower(motorPower);
+        telemetry.addData("Motors", "The robot's left motor is running at " + -motorPower + " for " + timeInMilliseconds + " milliseconds.");
+        telemetry.addData("Motors", "The robot's right motor is running at " + motorPower + " for " + timeInMilliseconds + " milliseconds.");
+        Thread.sleep(timeInMilliseconds);
+    }
+    public void stop_driving() {
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+        telemetry.addData("Status", "The robot has finished moving!");
     }
 }
