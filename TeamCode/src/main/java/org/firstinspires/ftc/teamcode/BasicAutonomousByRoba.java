a34c8baa70117978c3d7f107dd188b6b31f8627d
Roba <abbajabal.roba@jdragonmail.us>
2017-10-15 20:38:07 -0500

    Roba pushed this.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomousByRoba.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomousByRoba.java
@@ -28,30 +28,30 @@ import com.qualcomm.robotcore.util.Range;
  *                 Start where it says "Begin editing code here." and end where it says "Stop editing code here." DO NOT edit any code outside of those two comments!
  */
 
-@Autonomous(name="AutonomousBlueLeftByRoba", group="AutonomousByRoba")
+@Autonomous(name="BasicAutonomousByRoba", group="AutonomousByRoba")
 //@Disabled
 
-public class AutonomousBlueLeftByRoba extends LinearOpMode {
+public class BasicAutonomousByRoba extends LinearOpMode {
 
-    private ElapsedTime runTime = new ElapsedTime(); //Creates a time for the amount of time driven on the drive station.
-    private DcMotor leftMotor = null; //Makes a variable called leftMotor.
-    private DcMotor rightMotor = null; //Makes a variable called  rightMotor.
-    private Servo leftClaw = null; //Makes a variable called leftClaw.
-    private Servo rightClaw = null; //Makes a variable called rightClaw.
+    private ElapsedTime runTime = new ElapsedTime();
+    private DcMotor leftMotor = null;
+    private DcMotor rightMotor = null;
+    private Servo leftClaw = null;
+    private Servo rightClaw = null;
 
     @Override
     public void runOpMode() throws InterruptedException{
 
         telemetry.addData("Status", "This script has been made by Roba! " + "The robot has initialized! ");
 
-        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor"); //Gets the drivers for the leftMotor.
-        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor"); //Gets the drivers for the rightMotor.
-        leftClaw = hardwareMap.get(Servo.class, "leftClaw"); //Gets the drivers for the leftServo.
+        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+        leftClaw = hardwareMap.get(Servo.class, "leftClaw");
         rightClaw = hardwareMap.get(Servo.class, "rightClaw");
-        leftMotor.setDirection(DcMotor.Direction.FORWARD); //Sets the direction of the motor.
-        rightMotor.setDirection(DcMotor.Direction.REVERSE); //Sets the direction of the motor.
-        leftClaw.setDirection(Servo.Direction.FORWARD); //Sets the direction of the servo.
-        rightClaw.setDirection(Servo.Direction.REVERSE); //Sets the direction of the servo.
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftClaw.setDirection(Servo.Direction.FORWARD);
+        rightClaw.setDirection(Servo.Direction.REVERSE);
 
 
         //Begin editing code here.
